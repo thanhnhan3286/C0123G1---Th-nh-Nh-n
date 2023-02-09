ce6d296cc7841aedbcb4a787269856bd7388030e
thanhnhan3286 <dothanhnhan362000@gmail.com>
2023-02-09 13:34:43 +0700

    first commit

--- ss7_bien_kieudulieu_toantu/Changemoeny.js
+++ ss7_bien_kieudulieu_toantu/Changemoeny.js
@@ -1,21 +1,24 @@
-function ChangeMoney() {
-    let Amount = document.getElementById("Amount").value;
-    let FromC = document.getElementById("From").value;
-    let To = document.getElementById("To").value;
-    let Result;
-
-
-    if (FromC == "USD" && To == "VND") {
-        Result = "Result: " + (Amount * 23000) + " D";
-    }
-    else if (FromC == "VND"&& To =="USD"){
-        Result = "Result: "+ (Amount / 23000) + " $";
-    }
-    else if (FromC == "VND") {
-        Result = "Result: " + Amount + " D"
-    } else {
-        Result = "Result: " + Amount + " $"
-    }
-
-    document.getElementById("Result").innerHTML = Result;
+switch (new Date().getDay()) {
+    case 0:
+        day = "Chủ Nhật";
+        break;
+    case 1:
+        day = "Thứ hai";
+        break;
+    case 2:
+        day = "Thứ ba";
+        break;
+    case 3:
+        day = "Thứ tư";
+        break;
+    case 4:
+        day = "Thứ năm";
+        break;
+    case 5:
+        day = "Thứ sáu";
+        break;
+    case 6:
+        day = "Thứ bảy";
+        break;
 }
+document.write(day);
